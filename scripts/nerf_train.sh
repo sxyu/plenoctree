364a7afef5ca75732780430419547fca02e4c5c5
Ruilong Li <397653553@qq.com>
2021-04-19 00:38:38 -0700

    rm scripts folder

--- scripts/nerf_train.sh
+++
@@ -1,9 +0,0 @@
-export DATA_ROOT=./data/NeRF/nerf_synthetic/
-export CKPT_ROOT=./checkpoints/plenoctree/
-export SCENE=chair
-export CONFIG_FILE=nerf_sh/config/blender
-
-python -m nerf_sh.train \
-    --train_dir $CKPT_ROOT/$SCENE/ \
-    --config $CONFIG_FILE \
-    --data_dir $DATA_ROOT/$SCENE/ 
