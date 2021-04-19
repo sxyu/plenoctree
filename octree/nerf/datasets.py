f7a81029f3181896d0c76ff84460dfed0996acb0
Ruilong Li <397653553@qq.com>
2021-04-19 01:31:57 -0700

    add copyrights

--- octree/nerf/datasets.py
+++ octree/nerf/datasets.py
@@ -1,5 +1,6 @@
 # coding=utf-8
-# Copyright 2021 The Google Research Authors.
+# Modifications Copyright 2021 The PlenOctree Authors.
+# Original Copyright 2021 The Google Research Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- octree/nerf/sh_proj.py
+++ octree/nerf/sh_proj.py
@@ -1,4 +1,20 @@
-"""Sperical Harmonics Related Functions
+# Modifications Copyright 2021 The PlenOctree Authors.
+# Original Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Sperical harmonics projection related functions
+
+Some codes are borrowed from:
 https://github.com/google/spherical-harmonics/blob/master/sh/spherical_harmonics.cc
 """
 from typing import Callable
